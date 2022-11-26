4cbb5af1cd5a176bee8171eeb90d632037f02ceb
Adwoa Addo-Brako <baahbrako@gmail.com>
2022-11-26 21:10:34 +0000

    adding JS functionality

--- app.js
+++ app.js
@@ -2,3 +2,15 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const button = document.querySelector(".modal-btn");
+const closeBtn = document.querySelector(".close-btn");
+const overlay = document.querySelector(".modal-overlay");
+
+button.addEventListener("click", () => {
+  overlay.classList.add("open-modal");
+});
+
+closeBtn.addEventListener("click", () => {
+  overlay.classList.remove("open-modal");
+});
